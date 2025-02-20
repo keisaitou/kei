6c8f61676c9b32bf6be678cd160e878c6c812a8e
keisaitou <keisaito65@gmail.com>
2025-02-20 14:38:46 +0900

    Add files via upload

--- contactOutput.php
+++ contactOutput.php
@@ -17,10 +17,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <p><?php echo htmlspecialchars($name); ?></p>
         <input type="hidden" name="user_name" value="<?php echo htmlspecialchars($name); ?>">
     </div>
+
     <div class="contactOutInner">
         <h3>フリガナ:</h3>
         <p><?php echo htmlspecialchars($yomi); ?></p>
-        <input type="hidden" name="user_neme_yomi" value="<?php echo htmlspecialchars($yomi); ?>">
+        <input type="hidden" name="user_name_yomi" value="<?php echo htmlspecialchars($yomi); ?>">
     </div>
 
     <div class="contactOutInner">
@@ -28,13 +29,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <p><?php echo htmlspecialchars($email); ?></p>
         <input type="hidden" name="user_mail" value="<?php echo htmlspecialchars($email); ?>">
     </div>
-    </div>
 
     <div class="contactOutInner">
         <h3>お問い合わせ内容:</h3>
         <p><?php echo nl2br(htmlspecialchars($message)); ?></p>
         <input type="hidden" name="message" value="<?php echo htmlspecialchars($message); ?>">
     </div>
+
     <div class="submitContainer">
         <button type="button" onclick="window.history.back();">戻る</button>
         <button type="submit">送信</button>
--- contactSend.php
+++ contactSend.php
@@ -1,15 +1,39 @@
+
 <?php
-$pegeTitle = '送信完了';
-require 'header.php';
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+     // フォームから送信されたデータを取得
+     $name = $_POST['user_name'];  // 名前
+     $yomi = $_POST['user_name_yomi'];  // フリガナ
+     $email = $_POST['user_mail'];  // メールアドレス
+     $message = $_POST['message'];  // メッセージ
+
+     // 私たちのメールアドレス
+    $yourEmail = "keisaito65@gmail.com";  // ここに自分のメールアドレスを入れる
+
+    // メールの内容
+    $subject = "お問い合わせ内容の確認";  // メールのタイトル
+    $messageBody = "お名前: " . $name . "\n" . "フリガナ: " . $yomi . "\n" . "メールアドレス: " . $email . "\n" . "メッセージ: " . $message;  // メールの本文
+}
+
+// メールを送る
+if (mail($yourEmail, $subject, $messageBody)) {
+    echo "お問い合わせ内容が送信されました。";
+} else {
+    echo "送信に失敗しました。";
+}
+
+
+// お客さんへのメール
+$clientSubject = "お問い合わせの確認";  // 件名
+$clientMessage ="この度はお問い合わせ誠に有難うございます。\n以下の内容で承りました。\n返信までしばらくお待ちください。\n\n"."お名前: " . $name . "\n" . "フリガナ: " . $yomi . "\n" . "メールアドレス: " . $email . "\n" . "メッセージ: " . $message. "\n\n\nURL:http://k3110.stars.ne.jp/portfolio/";  // メールの内容
+
+// クライアントへのメール送信
+if (mail($email, $clientSubject, $clientMessage)) {
+    // クライアントへの確認メールが送信された場合、thankYou.phpにリダイレクト
+    header('Location: thankYou.php');  // 「送信ありがとうございます」のページに移動
+    exit();  // リダイレクト後にスクリプトを終了
+} else {
+    echo "確認メールの送信に失敗しました。";
+}
+
 ?>
-
-<main>
-    <div class="contactSend">
-        <div class="contactSendinner">
-            <p>お問い合わせ有難うございます。<br>返信までしばらくお待ちください</p>
-        </div>
-        <a href="index.php">トップへ戻る</a>
-    </div>
-</main>
-
-<?php require 'footer.php'; ?>
